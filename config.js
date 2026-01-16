b533402a4e4c6536db24a1c0f818f8696ab6e4fa
Lawson <liangsi.lu@outlook.com>
2026-01-17 02:33:48 +0800

    init

---
+++ config.js
@@ -0,0 +1,32 @@
+window.SITE_CONFIG = {
+  info: {
+    picture: "img/person/0.JPG",
+    name: "Liangsi Lu",
+    title: "Undergraduate Student",
+    school: "Guangdong University of Technology",
+    intrest: "Representation Learning, Image Generation",
+    email: "lu.liangsi.cn@gmail.com",
+    github: "https://github.com/LuLiangsi",
+    google_scholar: "https://scholar.google.com/citations?user=XZSJYAEAAAAJ&hl=en"
+  },
+  about: "Hello! I am Liangsi Lu, currently an undergraduate student majoring in Information and Computing Science at Guangdong University of Technology. My academic interests lie in Computer Vision. I am particularly passionate about Representation Learning and Image Generation.\n\nI am actively seeking Ph.D. opportunities for Fall 2027.",
+  select_publications: [
+    {
+      title: "Riemannian Liquid Spatio-Temporal Graph Network",
+      authors: ["Liangsi Lu", "Jingchao Wang", "Zhaorong Dai", "Hanqian Liu", "Yang Shi"],
+      venue: "WWW 2026",
+      picture: "img/publications/01_rlstg.png",
+      page: "https://rlstg.github.io",
+      paper: "",
+      code: "https://github.com/RLSTG/RLSTG"
+    },
+    {
+      title: "PC-UNet: An Enforcing Poisson Statistics U-Net for Positron Emission Tomography Denoising",
+      authors: ["Yang Shi", "Jingchao Wang", "Liangsi Lu", "et al."],
+      venue: "BIBM 2025",
+      picture: "img/publications/00_pcunt.png",
+      paper: "https://arxiv.org/abs/2510.14995",
+      code: ""
+    }
+  ]
+};
