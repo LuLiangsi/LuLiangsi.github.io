8a827c1a9bd84ecb795b126b0fbe3535d5e63755
Lawson <liangsi.lu@outlook.com>
2026-02-08 22:49:58 +0800

    update

--- config.js
+++ config.js
@@ -1,15 +1,4 @@
 window.SITE_CONFIG = {
-  info: {
-    picture: "img/person/0.JPG",
-    name: "Liangsi Lu",
-    chinese_name: "卢梁司",
-    title: "Undergraduate Student",
-    school: "Guangdong University of Technology",
-    intrest: "Representation Learning, Image Generation",
-    email: "lu.liangsi.cn@gmail.com",
-    github: "https://github.com/LuLiangsi",
-    google_scholar: "https://scholar.google.com/citations?user=XZSJYAEAAAAJ&hl=en"
-  },
   about: "Hello! I am Liangsi Lu, currently an undergraduate student majoring in Information and Computing Science at Guangdong University of Technology. My academic interests lie in Computer Vision. I am particularly passionate about Representation Learning and Image Generation.\n\nI am actively seeking Ph.D. opportunities for Fall 2027.",
   select_publications: [
     {
@@ -18,15 +7,23 @@ window.SITE_CONFIG = {
       venue: "WWW 2026",
       picture: "img/publications/01_rlstg.png",
       page: "https://rlstg.github.io",
-      paper: "",
+      paper: "https://arxiv.org/pdf/2601.14115",
       code: "https://github.com/RLSTG/RLSTG"
     },
     {
+      title: "MMErroR: A Benchmark for Erroneous Reasoning in Vision-Language Models",
+      authors: ["Yang Shi", "Yifeng Xie", "Minzhe Guo", "Liangsi Lu", "Mingxuan Huang", "Jingchao Wang", "Zhihong Zhu", "Boyan Xu", "Zhiqi Huang"],
+      venue: "arXiv",
+      picture: "img/publications/02_MMErroR.png",
+      page: "https://mmerror-benchmark.github.io",
+      paper: "https://arxiv.org/pdf/2601.03331",
+    },
+    {
       title: "PC-UNet: An Enforcing Poisson Statistics U-Net for Positron Emission Tomography Denoising",
-      authors: ["Yang Shi", "Jingchao Wang", "Liangsi Lu", "et al."],
+      authors: ["Yang Shi", "Jingchao Wang", "Liangsi Lu", "Mingxuan Huang", "Ruixin He", "Yifeng Xie", "Hanqian Liu", "Minzhe Guo", "Yangyang Liang", "Weipeng Zhang", "Zimeng Li", "Xuhang Chen"],
       venue: "BIBM 2025",
       picture: "img/publications/00_pcunt.png",
-      paper: "https://arxiv.org/abs/2510.14995",
+      paper: "https://arxiv.org/pdf/2510.14995",
       code: ""
     }
   ]
